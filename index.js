67fabb40751e653e59ff25aece5ddfb0c41d1473
Clarence Jordan <clarencejordan087@gmail.com>
2023-07-11 22:04:10 -0700

    almsot done I need to work on other assighnments Ill come back  to this later

--- index.js
+++ index.js
@@ -256,7 +256,7 @@ else if(theNumber>=100){
     console.log("big number")
 }
 
-console.log('=============================================')
+
 
 
 /* 
@@ -285,3 +285,167 @@ else{
 }
 
 
+//!H. what's in your closet?
+
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "Per Scholas hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+  
+  // Thom's closet is more complicated. Check out this nested data structure!!
+  const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
+
+  //?What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+
+const kristynsThird =  kristynsCloset[3]
+
+console.log(`Kristyn is rocking that ${kristynsThird}`)
+
+
+//? Kristyn just bought some sweet shades! Add "raybans"to her closet after "yellow knit hat".
+
+kristynsCloset.splice(5,1, "stained knit hat")
+console.table(kristynsCloset)
+
+//?Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirtsarray.
+
+const shirt = thomsCloset[0][0]
+
+
+//? In the same way, access one item from Thom's pants array.
+
+const pants = thomsCloset[1][1]
+
+//?Access one item from Thom's accessories array.
+
+const accessories = thomsCloset[2][1]
+
+//?Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+
+console.log(`Thom is looking fierce in a ${shirt},${pants},${accessories} `)
+
+//?Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+
+
+thomsCloset[1].splice(2,1, "Footie Pajamas")
+console.table(thomsCloset)
+
+
+//!IV.Functions
+
+
+//!B.printCool
+
+
+function printCool(name){
+    return `${name} is cool`
+}
+
+console.log(printCool("Captain Reynolds"))
+
+
+
+//!C.calculatecube
+
+//?Write a function calculateCubethat takes a single number and prints the volume of a cube made from that number.
+
+function calculateCube(valume){
+    return valume*valume*valume
+}
+console.log(calculateCube(5))
+
+//!D. isVowel
+
+function isVawel(char){
+
+
+    let lowercase=char.toLowerCase()
+    if('a'=== lowercase||'e'=== lowercase|| 'i'=== lowercase||'o'=== lowercase||'u'=== lowercase||'y'=== lowercase){
+
+        return true
+        
+    }
+    else{
+        return false
+    }
+}
+
+
+console.log(isVawel('x'))
+
+
+
+//!getTwoLengths
+
+
+function getTwoLengths(str1,str2){
+    let array = [str1.length,str2.length]
+    return array  
+}
+
+
+console.log(getTwoLengths('length','growth'))
+
+
+
+//!F getMultipleLengths
+function getMultipleLengths(num){
+
+    let lenghtof = []
+    for(let i = 0; i<=num.length;i++){
+        lenghtof.push(i)
+    }
+
+    return lenghtof
+
+}
+console.log(getMultipleLengths(["hello", "what", "is", "up", "dude"]))
+
+
+//!G maxOfThree
+
+
+function maxOfThree(arr,arr1,arr2){
+   return Math.max(arr,arr1,arr2)
+}
+
+
+
+console.log(maxOfThree(6, 9, 1))
+
+
+//!H printLongestWorld
+function printLongestWorld(arr){
+    for(let i = 0;i<=arr.length;i++){
+        let exit=  Math.max(arr[i])
+        return exit
+    }
+    return exit
+   
+}
+console.log(printLongestWorld(["BoJack", "Princess", "Diane", "a", "Max", "Peanutbutter", "big", "Todd"]))
+
+//TODO need to finish this returning NaN
